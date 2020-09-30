7d009da384e0ccaa5248a2c9840d5c6f9c33b541
OpHacker <64748174+KalidOp@users.noreply.github.com>
2020-09-30 13:49:56 +0300

    Add files via upload

---
+++ NumScan.py
@@ -0,0 +1,167 @@
+def NumScan():
+    # Coded By OpHacker & Lord <3 
+    import os
+
+    import time
+
+    import sys
+
+    def print_slow(str):
+        for letter in str:
+            sys.stdout.write(letter)
+            sys.stdout.flush()
+            time.sleep(0.05)
+    try:
+        import random
+    except:
+        os.system("pip install random")
+    try:
+        import phonenumbers
+    except:
+        os.system("pip install phonenumbers")
+
+    from phonenumbers import geocoder
+
+    from phonenumbers import carrier
+    try:
+        import requests
+    except:
+        os.system("pip install requests")
+    from time import sleep
+    try:
+        import subprocess
+    except:
+        os.system('pip install subprocess')
+
+    Yellow = "\033[0;32m"
+
+    Blue = "\033[0;34m"
+    #Clear
+    name = os.name
+
+    if name == "nt":
+        os.system('cls')
+    else:
+        ooo = subprocess.getoutput('users')
+        os.system('clear')
+    sleep(1)
+    print(Blue+'''
+▀█▄   ▀█▀                      ▄█▀▀▀▄█                           
+ █▀█   █  ▄▄▄ ▄▄▄  ▄▄ ▄▄ ▄▄    ██▄▄  ▀    ▄▄▄▄   ▄▄▄▄   ▄▄ ▄▄▄   
+ █ ▀█▄ █   ██  ██   ██ ██ ██    ▀▀███▄  ▄█   ▀▀ ▀▀ ▄██   ██  ██  
+ █   ███   ██  ██   ██ ██ ██  ▄     ▀██ ██      ▄█▀ ██   ██  ██  
+▄█▄   ▀█   ▀█▄▄▀█▄ ▄██ ██ ██▄ █▀▄▄▄▄█▀   ▀█▄▄▄▀ ▀█▄▄▀█▀ ▄██▄ ██▄''')
+    print_slow(Yellow+"    instagram ---> @t8qu_ & @00017z")
+    print("")
+    print_slow("    + NumScan V2.0 +")
+    print("")
+    sleep(0.4)
+    print(f"    Welcome {ooo}")
+    print("")
+    try:
+        if requests.get('https://google.com').ok:
+            pass
+    except:
+        print("\033[0;32m[\033[0;31m-\033[0;32m]\033[0;36m Please Check You Internet Connection")
+        sleep(1)
+        exit()
+    print("")
+    country_code = input(f"\033[0;32m[\033[0;31m+\033[0;32m]{Blue} Enter Country Code ex ---> SA : ")
+    print("")
+    country_code2 = input("\033[0;32m[\033[0;31m*\033[0;32m] Enter Country Code ex ---> +966 : ")
+    print("")
+    Target_Num = input("\033[0;32m[\033[0;31m+\033[0;32m] Enter Your Target Number : ")
+    r = requests.get(
+        f"http://146.148.112.105/caller/index.php/UserManagement/search_number?number={Target_Num}&country_code={country_code}")
+    data = r.json()
+    if ('"response":"2","msg":"No recourd found."') in r.text:
+        print("")
+        print("\033[0;32m[\033[0;31mx\033[0;32m]\033[0;31m Number Not Found On Website...")
+    if ('"response":"0","msg":"Record found.') in r.text:
+        for r in data['result']:
+            print("")
+            print("\033[0;32m[\033[0;31m✔\033[0;32m]\033[0;36m Number Found Get information...")
+            print("")
+            sleep(2)
+            print("\033[0;32m[\033[0;31mi\033[0;32m]\033[0;36m Name : " + r['name'])
+            print("")
+            print("\033[0;32m[\033[0;31mi\033[0;32m]\033[0;36m PhoneNumber : " + r['number'])
+            print("")
+            print("\033[0;32m[\033[0;31mi\033[0;32m]\033[0;36m Country Code : " + r['country_code'])
+            print("")
+            print("\033[0;32m[\033[0;31mi\033[0;32m]\033[0;36m ID : " + r['id'])
+    try:
+        all1 = country_code2 + Target_Num
+
+        ro_number = phonenumbers.parse(all1, "en")
+
+        Service = carrier.name_for_number(ro_number, "en")
+
+        Gdd = geocoder.description_for_number(ro_number, "en")
+        print("")
+        print("\033[0;32m[\033[0;31mi\033[0;32m] Carrier : ", Service)
+        print("")
+        print("\033[0;32m[\033[0;31mi\033[0;32m] Country Name : ", Gdd)
+    except:
+        print("")
+        print("\033[0;32m[\033[0;31mx\033[0;31m] Number Not Found...")
+    requests.session()
+    head = {
+        'accept': '*/*',
+        'accept-encoding': 'gzip, deflate, br',
+        'accept-language': 'en-US,en;q=0.9,ar;q=0.8',
+        'content-length': '55',
+        'content-type': 'application/x-www-form-urlencoded',
+        'cookie': 'ig_did=C85526B3-D56D-4C62-9955-A0FEDCEEF1B2; mid=XtRWJAALAAGDUx-1-NNLph8FvNQS; shbid=13522; shbts=1593969544.8718596; rur=FRC; csrftoken=92L3g6joMsjBqf61UOsQmjpoHf3reSFv; urlgen="{\"2001:16a2:1339:aa00:4c09:f79e:5aa3:bca3\": 25019}:1jsLuJ:SeoqLoKY8TPu7D3BCIcgfDXOm7Q"',
+        'dnt': '1',
+        'origin': 'https://www.instagram.com',
+        'referer': 'https://www.instagram.com/accounts/password/reset/',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-origin',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36',
+        'x-csrftoken': '92L3g6joMsjBqf61UOsQmjpoHf3reSFv',
+        'x-ig-app-id': '936619743392459',
+        'x-ig-www-claim': 'hmac.AR20oU9cC9AGn6Sd8PyDrHogb3ZFG9fAWLqSuqNBSRHpj0Ll',
+        'x-instagram-ajax': 'fc31028544fb',
+        'x-requested-with': 'XMLHttpRequest'
+    }
+    url = "https://www.instagram.com/accounts/account_recovery_send_ajax/"
+    data = {
+        'email_or_username': Target_Num
+    }
+    payload = requests.post(url, data=data, headers=head).text
+    js = payload
+    all = Target_Num
+    if ('"email_or_sms_sent": true') in js:
+        print("")
+        print(f"\033[0;32m[\033[0;31mi\033[0;32m]\033[1;32m Number Found On instagram ---> {Target_Num}")
+    if ('"message": "No users found"') in js:
+        print("")
+        print(f"\033[0;32m[\033[0;31mi\033[0;32m]\033[0;31m Number Not Found On instagram ---> {Target_Num}")
+    if ('"message": "We have encountered an error while resetting your password. Please try again."') in js:
+        print("")
+        print("\033[0;32m[\033[0;31mx\033[0;32m]\033[1;31m Instagram Error ---> We have encountered an error while resetting your password. Please try again.")
+    print("")
+
+
+
+    Re = input("\033[0;32m[\033[0;31m+\033[0;32m] again ? (y/n): ")
+    if Re == "y":
+        NumScan()
+    elif Re == "Y":
+        NumScan()
+    elif Re == "n":
+        print("")
+        print("\033[0;32m[\033[0;31m+\033[0;32m] Good Bye...")
+        exit()
+    elif Re == "N":
+        print("")
+        print("\033[0;32m[\033[0;31m+\033[0;32m] Good Bye...")
+        exit()
+    else:
+        print("")
+        print(f"\033[0;31m[\033[0;31m-\033[0;31m] Command Not Found...! {Re}")
+        exit()
+
+NumScan()
